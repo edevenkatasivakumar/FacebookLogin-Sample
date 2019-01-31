cafe24589d3f2f3137f96ffa91fb933e8993f8cb
sivakumar <sivainvitesyou@gmail.com>
2019-01-31 12:58:34 +0530

    Initial Commit

---
+++ FacebookLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookLogin
+//
+//  Created by TechMahindra on 31/01/19.
+//  Copyright © 2019 Sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
