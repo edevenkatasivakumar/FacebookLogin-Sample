53a08f52056312be389ff8f396853edf0a2005a6
sivakumar <sivainvitesyou@gmail.com>
2019-01-31 15:15:14 +0530

    Facebook Login implemented.

--- FacebookLogin/AppDelegate.swift
+++ FacebookLogin/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: nil)
+    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- FacebookLogin/ViewController.swift
+++ FacebookLogin/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
-
+import FBSDKCoreKit
+import FBSDKLoginKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        btnFBLoginClicked()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func btnFBLoginClicked() {
+        let loginManager : FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logIn(withReadPermissions: ["public_profile","email"], from: self) { (result, error) in
+            if error == nil {
+                let strToken : String = (result?.token.tokenString)!
+                print("Easy Solutions Web's FB Token = \(strToken)")
+                DispatchQueue.main.async {
+                   // self.btnFbLogin.titleLabel?.text = "Logout User"
+                }
+            }
+        }
+    }
 
 }
 
